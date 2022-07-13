6ce30d2c17f71e207646194004efd6cbdc914759
Matt Wright <54322905+mattwr1ght@users.noreply.github.com>
2022-07-12 21:02:42 -0700

    Route for signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "movies#index"
   resources :movies do
     resources :reviews
   end
   
+  get "/signup", to: 'users#new', as: 'signup'
+
+
 end
