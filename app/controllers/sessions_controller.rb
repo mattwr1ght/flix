5d6995077a3b0ac4f2f5478d13fbc5ab48fd7196
Matt Wright <54322905+mattwr1ght@users.noreply.github.com>
2022-07-13 14:34:36 -0700

    Route for Session and create implemented

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+    def new
+
+    end
+
+    def create
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
+            redirect_to user, notice: "Welcome back, #{user.name}!"
+        else
+            flash.now[:alert] = "The email or password is invalid. Please try again."
+            render :new
+        end
+
+    end
+
+    def destroy
+
+    end
+
+end
