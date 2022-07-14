610e32e44bdadfd4a67db639df2910596bb76399
Matt Wright <54322905+mattwr1ght@users.noreply.github.com>
2022-07-14 14:32:22 -0700

    Add has many to Movie, User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :reviews, dependent: :destroy
-
+  has_many :favorites, dependent: :destroy
+  
   validates :name, presence: true
   validates :email, presence: true,
                    format: { with: /\S+@\S+/ },
