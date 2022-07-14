5508a6821e647150716bfb384e4dd5891a83fbfa
Matt Wright <54322905+mattwr1ght@users.noreply.github.com>
2022-07-14 11:35:21 -0700

    Update User and Review models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :reviews, dependant: :destroy
+
   validates :name, presence: true
   validates :email, presence: true,
                    format: { with: /\S+@\S+/ },
