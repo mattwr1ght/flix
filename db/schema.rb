77d2f588583573a1126c944bf2c253f39d009ad3
Matt Wright <54322905+mattwr1ght@users.noreply.github.com>
2022-07-15 10:25:45 -0700

    Migration to add Characterizations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_15_172210) do
+ActiveRecord::Schema.define(version: 2022_07_15_172458) do
+
+  create_table "characterizations", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["genre_id"], name: "index_characterizations_on_genre_id"
+    t.index ["movie_id"], name: "index_characterizations_on_movie_id"
+  end
 
   create_table "favorites", force: :cascade do |t|
     t.integer "movie_id", null: false
@@ -59,6 +68,8 @@ ActiveRecord::Schema.define(version: 2022_07_15_172210) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "characterizations", "genres"
+  add_foreign_key "characterizations", "movies"
   add_foreign_key "favorites", "movies"
   add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "movies"
