7887b9dc3aa79e1b71738d13787b0b31a0d1b381
Matt Wright <54322905+mattwr1ght@users.noreply.github.com>
2022-07-15 16:26:59 -0700

    Create Slug methods in Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
 
+    before_save :set_slug
+
     has_many :reviews, dependent: :destroy
     has_many :favorites, dependent: :destroy
     has_many :fans, through: :favorites, source: :user
@@ -8,7 +10,8 @@ class Movie < ApplicationRecord
     
     RATINGS = %w(G PG PG-13 R NC-17)
 
-    validates :title, :released_on, :duration, presence: true
+    validates :title, presence: true, uniqueness: true
+    validates :released_on, :duration, presence: true
     validates :description, length: { minimum: 25 }
     validates :total_gross, numericality: { greater_than_or_equal_to: 0 } 
     validates :image_file_name, format: {
@@ -28,4 +31,14 @@ class Movie < ApplicationRecord
     def flop?
         total_gross.blank? || total_gross < 225_000_000
     end
+
+    def to_param
+        slug
+    end
+
+    private
+
+    def set_slug
+        self.slug = title.parameterize
+    end 
 end
